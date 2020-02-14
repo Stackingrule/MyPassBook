e65ec82af9bde520fc522b8770a970e8db4583b0
Stackingrule <messageswitching@gmail.com>
2020-02-14 22:18:40 +0800

    init Merchants

---
+++ merchants/src/test/java/com/stackingrule/passbook/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackingrule.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
