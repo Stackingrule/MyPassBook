4ae67fbc29581599b5e50944cd7d1fbf31e9720b
Stackingrule <messageswitching@gmail.com>
2020-02-15 18:16:16 +0800

    init Passbook Project

---
+++ passbook/src/test/java/com/stackingrule/passbook/PassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackingrule.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
