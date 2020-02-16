2d8fb9fa7731140df751460ecf0be076d495b949
Stackingrule <messageswitching@gmail.com>
2020-02-16 22:19:04 +0800

    add HBase Pass 表 RowKey 生成

--- passbook/src/main/java/com/stackingrule/passbook/utils/RowKeyGenUtil.java
+++ passbook/src/main/java/com/stackingrule/passbook/utils/RowKeyGenUtil.java
@@ -1,6 +1,7 @@
 package com.stackingrule.passbook.utils;
 
 import com.stackingrule.passbook.vo.Feedback;
+import com.stackingrule.passbook.vo.GainPassTemplateRequest;
 import com.stackingrule.passbook.vo.PassTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -24,6 +25,20 @@ public class RowKeyGenUtil {
 
         return rowKey;
     }
+
+    /**
+     * <h2>根据提供的领取优惠卷请求生成 RowKey, 只可以在领取优惠卷时使用</h2>
+     * Pass RowKey = reversed(userId) + inverse(timestamp) + PassTemplate RowKey
+     * @param request {@link GainPassTemplateRequest}
+     * @return String RowKey
+     */
+    public static String genPassRowKey(GainPassTemplateRequest request) {
+
+        return new StringBuilder(String.valueOf(request.getUserId())).reverse().toString()
+                + (Long.MAX_VALUE - System.currentTimeMillis())
+                + genPassTemplateRowKey(request.getPassTemplate());
+    }
+
 
     /**
      * <h2>根据提供的 Feedback 对象生成 RowKey </h2>
