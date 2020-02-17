f83372b05ade216913d28ad724526303530e07b4
Stackingrule <messageswitching@gmail.com>
2020-02-17 13:56:53 +0800

    add Token 上传 Controller 实现

--- passbook/src/main/java/com/stackingrule/passbook/controller/TokenUploadController.java
+++ passbook/src/main/java/com/stackingrule/passbook/controller/TokenUploadController.java
@@ -1,15 +1,23 @@
 package com.stackingrule.passbook.controller;
 
+import com.stackingrule.passbook.constant.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,6 +35,54 @@ public class TokenUploadController {
     @Autowired
     public TokenUploadController(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
+    }
+
+    /**
+     * <h1></h1>
+     * @return
+     */
+    @GetMapping("/upload")
+    public String upload() {
+
+        return "upload";
+    }
+
+    @PostMapping("/token")
+    public String tokenFileUpload(@RequestParam("merchantsId") String merchantsId,
+                                  @RequestParam("passTemplateId") String passTemplateId,
+                                  @RequestParam("file") MultipartFile file,
+                                  RedirectAttributes redirectAttributes) {
+
+        if (null == passTemplateId || file.isEmpty()) {
+            redirectAttributes.addAttribute("message", "passTemplateId is null or file id empty");
+
+            return "redirect:/uploadStatus";
+        }
+
+        try {
+            File cur = new File(Constants.TOKEN_DIR + merchantsId);
+            if (!cur.exists()) {
+                log.info("Create File : {}", cur.mkdir());
+            }
+            Path path = Paths.get(Constants.TOKEN_DIR, merchantsId, passTemplateId);
+            Files.write(path, file.getBytes());
+
+            if (!writeTokenToRedis(path, passTemplateId)) {
+                redirectAttributes.addFlashAttribute("message", "write Token error");
+            } else {
+                redirectAttributes.addFlashAttribute("message", "You successfully uploaded '" + file.getOriginalFilename() + "'");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+
+        return "uploadStatus";
     }
 
     /**
