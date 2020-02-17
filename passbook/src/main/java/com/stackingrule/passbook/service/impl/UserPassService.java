2584a1e4e066b23fa421e52ce5a4c12878f640ba
Stackingrule <messageswitching@gmail.com>
2020-02-17 09:55:03 +0800

    add 用户优惠卷状态信息过滤功能实现

--- passbook/src/main/java/com/stackingrule/passbook/service/impl/UserPassService.java
+++ passbook/src/main/java/com/stackingrule/passbook/service/impl/UserPassService.java
@@ -2,15 +2,23 @@ package com.stackingrule.passbook.service.impl;
 
 import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
 import com.stackingrule.passbook.constant.Constants;
+import com.stackingrule.passbook.constant.PassStatus;
 import com.stackingrule.passbook.dao.MerchantsDao;
 import com.stackingrule.passbook.entity.Merchants;
+import com.stackingrule.passbook.mapper.PassRowMapper;
 import com.stackingrule.passbook.service.IUserPassService;
 import com.stackingrule.passbook.vo.Pass;
+import com.stackingrule.passbook.vo.PassInfo;
 import com.stackingrule.passbook.vo.PassTemplate;
 import com.stackingrule.passbook.vo.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.PrefixFilter;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.http.client.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +32,7 @@ import java.util.stream.Collectors;
 /**
  * <h1>用户优惠卷相关功能实现</h1>
  */
+@Slf4j
 public class UserPassService implements IUserPassService {
 
     /** HBase 客户端 **/
@@ -58,6 +67,60 @@ public class UserPassService implements IUserPassService {
         return null;
     }
 
+    /**
+     * <h1>根据优惠卷状态获取优惠卷信息</h1>
+     * @param userId 用户 id
+     * @param status {@link PassStatus}
+     * @return {@link Response}
+     * @throws Exception
+     */
+    private Response getPassInfoByStatus(Long userId, PassStatus status) throws Exception {
+
+        // 根据 userId 构建行键前缀
+        byte[] rowPrefix = Bytes.toBytes(new StringBuilder(String.valueOf(userId)).reverse().toString());
+
+        CompareFilter.CompareOp compareOp = status ==
+                PassStatus.UNUSED ?
+                CompareFilter.CompareOp.EQUAL : CompareFilter.CompareOp.NOT_EQUAL;
+        Scan scan = new Scan();
+        // 1. 行键前缀过滤器，找到特定用户优惠卷
+        scan.setFilter(new PrefixFilter(rowPrefix));
+        // 2. 基于列单元的过滤器， 找到未使用的优惠卷
+        if (status != PassStatus.ALL) {
+            scan.setFilter(new SingleColumnValueFilter(
+                    Constants.PassTable.FAMILY_I.getBytes(),
+                    Constants.PassTable.CON_DATE.getBytes(),
+                    compareOp,
+                    Bytes.toBytes("-1")));
+        }
+
+        List<Pass> passes = hbaseTemplate.find(Constants.PassTable.TABLE_NAME, scan, new PassRowMapper());
+        Map<String, PassTemplate> passTemplateMap = buildPassTemplateMap(passes);
+        Map<Integer, Merchants> merchantsMap = buildMerchantsMap(
+                new ArrayList<>(passTemplateMap.values()));
+
+        List<PassInfo> result = new ArrayList<>();
+
+        for (Pass pass : passes) {
+            PassTemplate passTemplate = passTemplateMap.getOrDefault(
+                    pass.getTemplateId(), null);
+            if (null == passTemplate) {
+                log.error("PassTemplate Null : {}", pass.getTemplateId());
+                continue;
+            }
+
+            Merchants merchants = merchantsMap.getOrDefault(
+                    passTemplate.getId(), null);
+            if (null == merchants) {
+                log.error("Merchants Null : {}", passTemplate.getId());
+                continue;
+            }
+
+            result.add(new PassInfo(pass, passTemplate, merchants));
+        }
+
+        return new Response(result);
+    }
     /**
      * <h2>通过获取的Pass对象构造Map</h2>
      * @param passes {@link Pass}
